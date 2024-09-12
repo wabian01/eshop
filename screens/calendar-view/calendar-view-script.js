8d56e062f6071cef90cdabf8593bca84ddeb9000
Nguyen Anh Do <donguyen@rtanalytics.vn>
2024-09-12 13:25:07 +0700

    Update code

--- screens/calendar-view/calendar-view-script.js
+++ screens/calendar-view/calendar-view-script.js
@@ -1,7 +1,7 @@
 Vue.component('calendar-view', {
         template: '#calendar-view',
         props: ['object','body_area','task','id_random','list_data_object'],
-        mixins: [handleButtonVisible],
+        mixins: [handleButtonVisible,handleDynamicButtons],
         data: function () {
             return {
                 item_content:"",
@@ -104,100 +104,7 @@ Vue.component('calendar-view', {
                 this.item_buttons = JSON.parse(button_replace);
                 this.handleDynamicButtons();
             },
-            replaceABDynamic(data,indexbutton,dynamic_buttons){
-                let that = this
-                data=data.sort((a,b)=>a.orderNumber-b.orderNumber)
-                data.forEach(dynamic_button => {
-                    if(dynamic_button.actionID!=""){
-                        dynamic_buttons.splice(indexbutton, 0, dynamic_button)
-                        indexbutton++;
-                    }
-                })
-                var itemJSONString = JSON.stringify(dynamic_buttons);
-                for (var key in that.list_item) {
-                    if (that.list_item.hasOwnProperty(key)) {
-                        if(that.list_item[key] == null){
-                            that.list_item[key] = "";
-                        }
-                        itemJSONString = itemJSONString.replace(new RegExp('##'+key+'##','g'),that.list_item[key].toString().replace(/[\r\n]+/g," "));
-                        itemJSONString = itemJSONString.replace('"','\"');
-                    }
-                }
-                for (var key in vm.flatRuntimeAttributes) {
-                    if (vm.flatRuntimeAttributes.hasOwnProperty(key)) {
-                        itemJSONString = itemJSONString.replace(new RegExp('##'+key+'##','g'),vm.flatRuntimeAttributes[key].replace(/[\r\n]+/g," "));
-                        itemJSONString = itemJSONString.replace('"','\"');
-                    }
-                }                
-                for(var key in vm.current.parent){
-                    try {
-                        itemJSONString = itemJSONString.replace(new RegExp('##'+key+'##','g'),vm.current.parent[key].replace(/[\r\n]+/g," "));
-                    } catch (error) {
-                    }
-                    itemJSONString = itemJSONString.replace('"','\"');
-                }
-                if(itemJSONString.indexOf('${getdata_dmobj')>-1){
-                        itemJSONString = vm.getDataDmobj(itemJSONString)
-                }
-                if(itemJSONString.indexOf('##source:dmobj')>-1){
-                    itemJSONString = vm.getSourceDmobj(itemJSONString)
-                }
-                itemJSONString = vm.jsonHolderData(itemJSONString,'buttonGroup')
-                if(itemJSONString.indexOf('##')>-1){
-                    itemJSONString = itemJSONString.replace(/\\\"##(.*?)##\\\"/g,"''");
-                    itemJSONString = itemJSONString.replace(/##(.*?)##/g,"''");
-                }                
-                that.item_buttons = JSON.parse(itemJSONString);
-            },
-            handleDynamicButtons: async function() {
-                let that = this;
-                let indexbutton = 0;
-                let item_buttons = this.item_buttons.sort((a,b)=>a.orderNumber-b.orderNumber);
-                let dynamic_buttons = JSON.parse(JSON.stringify(item_buttons));  
-                let p = $.when();
-                item_buttons.forEach((button,index) => {
-                    p = p.then(function() { 
-                        if(button.type=="dynamic"){
-                            if(indexbutton === 0){
-                                indexbutton = index;
-                            }
-                            if(button.hasOwnProperty('payloadLifetime')){
-                                if(vm.payloadLifetime.hasOwnProperty(hashCode(button.source))){
-                                    let timeCache = vm.payloadLifetime[hashCode(button.source)].time
-                                    let timePayload = button.payloadLifetime * 1000
-                                    let timeCurrent = (new Date()).getTime()
-                                    if(timeCurrent - timeCache < timePayload){
-                                        let button_cache = vm.payloadLifetime[hashCode(button.source)].buttons;
-                                        that.replaceABDynamic(button_cache,indexbutton,dynamic_buttons)
-                                        indexbutton = indexbutton + button_cache.length
-                                        return;
-                                    }
-                                }
-                            }
-                        return    $.ajax({
-                                url: button.source.replace("./","<?php echo AppEnv::BASE_URL ?>"+"/"),
-                                type: "GET",
-                                contentType: "application/json",
-                                dataType: "json",
-                                success: function (data) {
-                                    if(typeof(data[0])==='string') return;
-                                    if(button.hasOwnProperty('payloadLifetime')){
-                                        vm.payloadLifetime[hashCode(button.source)] = {
-                                            time: (new Date()).getTime(),
-                                            buttons: data
-                                        }
-                                    }
-                                    that.replaceABDynamic(data,indexbutton,dynamic_buttons)
-                                    indexbutton = indexbutton + data.length
-
-                                }, error: function (error) {
-                                    toastr.error(error);
-                                }
-                            });
-                        }                   
-                    })  
-                })   
-            },
+            
             onClickChild (event) {
                 this.onEventClick(event);
             },
--- screens/map-leaflet/map-leaflet-script.js
+++ screens/map-leaflet/map-leaflet-script.js
@@ -1,7 +1,7 @@
 Vue.component('map-leaflet', {
         template: '#map-leaflet',
         props: ['list_items_origin','id_random','body_area','item_search_string','task','object','item_filter_attributes','update_map','refresh_rate'],
-        mixins: [handleFilter,handleButtonVisible],
+        mixins: [handleFilter,handleButtonVisible,handleDynamicButtons],
         data: function () {
         return {
             searchs: [],
@@ -381,101 +381,6 @@ Vue.component('map-leaflet', {
                             
                         }
                     });  
-                },
-                replaceABDynamic(data,indexbutton,dynamic_buttons){
-                    let that = this
-                    data=data.sort((a,b)=>a.orderNumber-b.orderNumber)
-                    data.forEach(dynamic_button => {
-                        if(dynamic_button.actionID!=""){
-                            dynamic_buttons.splice(indexbutton, 0, dynamic_button)
-                            indexbutton++;
-                        }
-                    })
-                    var itemJSONString = JSON.stringify(dynamic_buttons);
-                    for (var key in that.list_item) {
-                        if (that.list_item.hasOwnProperty(key)) {
-                            if(that.list_item[key] == null){
-                                that.list_item[key] = "";
-                            }
-                            itemJSONString = itemJSONString.replace(new RegExp('##'+key+'##','g'),that.list_item[key].toString().replace(/[\r\n]+/g," "));
-                            itemJSONString = itemJSONString.replace('"','\"');
-                        }
-                    }
-                    for (var key in vm.flatRuntimeAttributes) {
-                        if (vm.flatRuntimeAttributes.hasOwnProperty(key)) {
-                            itemJSONString = itemJSONString.replace(new RegExp('##'+key+'##','g'),vm.flatRuntimeAttributes[key].replace(/[\r\n]+/g," "));
-                            itemJSONString = itemJSONString.replace('"','\"');
-                        }
-                    }                
-                    for(var key in vm.current.parent){
-                        try {
-                            itemJSONString = itemJSONString.replace(new RegExp('##'+key+'##','g'),vm.current.parent[key].replace(/[\r\n]+/g," "));
-                        } catch (error) {
-                        }
-                        itemJSONString = itemJSONString.replace('"','\"');
-                    }
-                    if(itemJSONString.indexOf('${getdata_dmobj')>-1){
-                            itemJSONString = vm.getDataDmobj(itemJSONString)
-                    }
-                    if(itemJSONString.indexOf('##source:dmobj')>-1){
-                        itemJSONString = vm.getSourceDmobj(itemJSONString)
-                    }
-                    itemJSONString = vm.jsonHolderData(itemJSONString,'buttonGroup')
-                    if(itemJSONString.indexOf('##')>-1){
-                        itemJSONString = itemJSONString.replace(/\\\"##(.*?)##\\\"/g,"''");
-                        itemJSONString = itemJSONString.replace(/##(.*?)##/g,"''");
-                    }                
-                    that.item_buttons = JSON.parse(itemJSONString);
-                },
-                // Refer at button-group
-                handleDynamicButtons: async function(){
-                    let that = this;
-                    let indexbutton = 0;
-                    let item_buttons = this.item_buttons.sort((a,b)=>a.orderNumber-b.orderNumber);
-                    let dynamic_buttons = JSON.parse(JSON.stringify(item_buttons));  
-                    let p = $.when();
-                    item_buttons.forEach((button,index) => {
-                        p = p.then(function() { 
-                            if(button.type=="dynamic"){
-                                if(indexbutton === 0){
-                                    indexbutton = index;
-                                }
-                                if(button.hasOwnProperty('payloadLifetime')){
-                                    if(vm.payloadLifetime.hasOwnProperty(hashCode(button.source))){
-                                        let timeCache = vm.payloadLifetime[hashCode(button.source)].time
-                                        let timePayload = button.payloadLifetime * 1000
-                                        let timeCurrent = (new Date()).getTime()
-                                        if(timeCurrent - timeCache < timePayload){
-                                            let button_cache = vm.payloadLifetime[hashCode(button.source)].buttons;
-                                            that.replaceABDynamic(button_cache,indexbutton,dynamic_buttons)
-                                            indexbutton = indexbutton + button_cache.length
-                                            return;
-                                        }
-                                    }
-                                }
-                                return $.ajax({
-                                    url: button.source.replace("./",window.location.origin+"/"),
-                                    type: "GET",
-                                    contentType: "application/json",
-                                    dataType: "json",
-                                    success: function (data) {
-                                        if(typeof(data[0])==='string') return;
-                                        if(button.hasOwnProperty('payloadLifetime')){
-                                            vm.payloadLifetime[hashCode(button.source)] = {
-                                                time: (new Date()).getTime(),
-                                                buttons: data
-                                            }
-                                        }
-                                        that.replaceABDynamic(data,indexbutton,dynamic_buttons)
-                                        indexbutton = indexbutton + data.length
-
-                                    }, error: function (error) {
-                                        toastr.error(error);
-                                    }
-                                });
-                            }                   
-                        })
-                    })     
                 },
                 buttonData() {
                     let button_replace_map = JSON.stringify(JSON.parse(JSON.stringify(attributes.item_buttons)))
