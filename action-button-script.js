fee79124d670e9fb50ffbc04810135fef9cef602
Nguyen Anh Do <donguyen@rtanalytics.vn>
2024-11-01 15:01:02 +0700

    Support icon set for action button

--- action-button-script.js
+++ action-button-script.js
@@ -501,11 +501,33 @@ Vue.component('action-button', {
                 this.item_button.label = this.item_button.name || this.item_button.title || ""
             }
 
-            this.renderButtonContent(font_icon, disabled, showBIcon, contentBIcon, bgBIcon, txtBIcon, borderColorBIcon, shadowBIcon)
+            this.renderButtonContent(button_description, font_icon, disabled, showBIcon, contentBIcon, bgBIcon, txtBIcon, borderColorBIcon, shadowBIcon)
             
             this.renderFloatButton()
         },
-        renderButtonContent(font_icon, disabled, showBIcon, contentBIcon, bgBIcon, txtBIcon, borderColorBIcon, shadowBIcon){
+        renderBigIcon(buttonDescription, fontIcon, disabled, iconSetUrl, iconSetStyle, iconSetFallBack){
+            let icon = ""
+            if(buttonDescription.hasOwnProperty('icon_color_background') && buttonDescription.icon_color_background != ""){
+                this.styleButton = 'style=\''+this.styleButton+" background-color:"+buttonDescription.icon_color_background+" !important;'"
+                icon += '<i class="isShowIcon '+fontIcon+'" aria-hidden="true" style="'+this.styleIconDisable+'color:black"></i> ';
+            }
+            else{
+                this.styleButton = 'style=\''+this.styleButton+this.styleAll+"'";
+                icon += '<i class="isShowIcon '+fontIcon+'" aria-hidden="true" style="'+this.styleIconDisable+'color:#00c5dc"></i> ';
+            }
+            if(fontIcon==='none'){
+                icon = ''
+            }
+            if(this.item_button.hasOwnProperty('imageUrl')){
+                iconfont = icon.replace('isShowIcon', 'isShowIcon d-none');
+                icon = '<img src="'+this.item_button.imageUrl+'" onerror="this.onerror=null;$(this).addClass(\'d-none\');$(this).next().removeClass(\'d-none\');" style="'+this.styleIconDisable+'width:1rem;"></i> '+iconfont;
+                if(iconSetUrl!==""){
+                    icon = `<img ${iconSetFallBack} src="${iconSetUrl}" style="${iconSetStyle};${this.styleIconDisable}width:1rem;"></i>`;
+                }
+            }
+            this.button_content = '<button '+disabled+' type="button" class="btn"'+this.styleButton+'>'+(buttonDescription.hasOwnProperty('show_icon') ? ((buttonDescription.show_icon == true || buttonDescription.show_icon == 'true') == true ? icon: '') :icon)+'</button><p style="'+this.styleAll+'overflow: hidden;text-overflow: ellipsis;margin: 6px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp:'+((buttonDescription.hasOwnProperty('max_text_line') && buttonDescription.max_text_line!=null)?buttonDescription.max_text_line:1)+';">'+(buttonDescription.hasOwnProperty('show_text') ? buttonDescription.show_text == true ? this.item_button['label']: '' :this.item_button['label'])+'</p>';
+        },
+        renderButtonContent(button_description, font_icon, disabled, showBIcon, contentBIcon, bgBIcon, txtBIcon, borderColorBIcon, shadowBIcon){
             let icon = "";
             let iconfont = "";
             let iconSetUrl = "";
@@ -516,26 +538,8 @@ Vue.component('action-button', {
             }
            
             if(this.styleButton != ""){
-                if(this.isBigIcon != ""){  
-                    if(button_description.hasOwnProperty('icon_color_background') && button_description.icon_color_background != ""){
-                        this.styleButton = 'style=\''+this.styleButton+" background-color:"+button_description.icon_color_background+" !important;'"
-                        icon += '<i class="isShowIcon '+font_icon+'" aria-hidden="true" style="'+this.styleIconDisable+'color:black"></i> ';
-                    }
-                    else{
-                        this.styleButton = 'style=\''+this.styleButton+this.styleAll+"'";
-                        icon += '<i class="isShowIcon '+font_icon+'" aria-hidden="true" style="'+this.styleIconDisable+'color:#00c5dc"></i> ';
-                    }
-                    if(font_icon==='none'){
-                        icon = ''
-                    }
-                    if(this.item_button.hasOwnProperty('imageUrl')){
-                        iconfont = icon.replace('isShowIcon', 'isShowIcon d-none');
-                        icon = '<img src="'+this.item_button.imageUrl+'" onerror="this.onerror=null;$(this).addClass(\'d-none\');$(this).next().removeClass(\'d-none\');" style="'+this.styleIconDisable+'width:1rem;"></i> '+iconfont;
-                        if(iconSetUrl!==""){
-                            icon = `<img ${iconSetFallBack} src="${iconSetUrl}" style="${iconSetStyle};${this.styleIconDisable}width:1rem;"></i>`;
-                        }
-                    }
-                    this.button_content = '<button '+disabled+' type="button" class="btn"'+this.styleButton+'>'+(button_description.hasOwnProperty('show_icon') ? ((button_description.show_icon == true || button_description.show_icon == 'true') == true ? icon: '') :icon)+'</button><p style="'+this.styleAll+'overflow: hidden;text-overflow: ellipsis;margin: 6px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp:'+((button_description.hasOwnProperty('max_text_line') && button_description.max_text_line!=null)?button_description.max_text_line:1)+';">'+(button_description.hasOwnProperty('show_text') ? button_description.show_text == true ? this.item_button['label']: '' :this.item_button['label'])+'</p>';
+                if(this.isBigIcon != ""){ 
+                    this.renderBigIcon(button_description, font_icon, disabled, iconSetUrl, iconSetStyle, iconSetFallBack)
                 }
                 else if(button_description.hasOwnProperty('type') && button_description.type === 'custom'){
                     if (this.item_button.hasOwnProperty("badge_icon") && this.item_button.badge_icon !== "") {
