cbe9634103e6268cfe1f033c4bcdce9de6bc32e6
Nguyen Anh Do <donguyen@rtanalytics.vn>
2024-11-28 14:16:32 +0700

    add html

--- html-view-script.js
+++ html-view-script.js
@@ -293,13 +293,12 @@
             },
             checkUrlIframe: function() {
                 this.loadURL = false
-                let domain = '*'
                 setTimeout(() => {
                     let frameEle = document.getElementById('url-' + this.id_random);
                     frameEle.contentWindow.postMessage({
                         function: 'on_init',
                         initPlatform: 'webapp'
-                    }, domain);
+                    });
                 }, 100);    
             },
             sortDataObject(data){
